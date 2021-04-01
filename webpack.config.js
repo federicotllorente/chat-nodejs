f3a5a68b3676c97cff4a402c56becd886ebc90d3
Federico Tejedor Llorente <federicotllorente@gmail.com>
2021-04-01 19:18:42 +0200

    'npm run start' command updated, and also the dependencies 'compression-webpack-plugin' and 'terser-webpack-plugin' were installed as Dev-dependencies to minimize the files size in order to optimize the App

--- webpack.config.js
+++ webpack.config.js
@@ -3,6 +3,8 @@ const webpack = require('webpack');
 const Dotenv = require('dotenv-webpack');
 const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CompressionWebpackPlugin = require('compression-webpack-plugin');
+const TerserPlugin = require('terser-webpack-plugin');
 
 const isDev = (process.env.NODE_ENV === 'development');
 let entry = ['./src/index.js'];
@@ -10,6 +12,7 @@ let entry = ['./src/index.js'];
 if (isDev) {
     // Refresh the page with a 2sec interval
     const clientConfig = 'webpack-hot-middleware/client?path=/__webpack_hmr&timeout=2000&reload=true';
+    // Add these settings only if it's in 'development' mode
     entry.push(clientConfig);
 }
 
@@ -20,6 +23,10 @@ module.exports = {
     output: {
         path: path.join(__dirname, '/public'),
         filename: 'app.js'
+    },
+    optimization: {
+        minimize: true,
+        minimizer: [new TerserPlugin()]
     },
     module: {
         rules: [
@@ -60,6 +67,10 @@ module.exports = {
     },
     plugins: [
         isDev ? new webpack.HotModuleReplacementPlugin() : () => { },
+        isDev ? () => { } : new CompressionWebpackPlugin({
+            test: /\.js$|\.css$/,
+            filename: '[path][base].gz'
+        }),
         new Dotenv(),
         new HtmlWebPackPlugin({
             hash: true,
